3e9729688d0400b684b674c7ee71f8b51aa6f55e
QsZyN <149283132+QsZyN@users.noreply.github.com>
2025-04-30 00:11:31 +0900

    modified:   api/main.py

--- api/main.py
+++ api/main.py
@@ -1,14 +1,15 @@
 # app.py
 from flask import Flask, redirect, abort, request
+import re
 
 app = Flask(__name__)
 
 
 @app.route("/<path:raw_string>")
 def get_raw_path(raw_string):
-    if "://" not in raw_string:
-        abort(400, description=f"Invalid URI: '://' is missing {request.url}")
-    return redirect(raw_string, code=307)
+    if ":/" not in raw_string:
+        abort(400, description=f"Invalid URI: '://' is missing {raw_string}")
+    return redirect(re.sub(r'(?<!:):/', r'://', raw_string), code=307)
 
 
 if __name__ == "__main__":
